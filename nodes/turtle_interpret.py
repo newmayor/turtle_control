2accd64a1845820fe29dce7adfb281b2966994e3
Numair Ahmed <numairahmed1@gmail.com>
2020-12-31 22:33:16 -0800

    completed first attempt and node turtle_interpret and defined both publisher and subscriber. still not sure if I'm doing this right. I guess I'll find out one day.

--- nodes/turtle_interpret.py
+++ nodes/turtle_interpret.py
@@ -1,24 +1,61 @@
-
+#!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
+import math
+import time
+from turtle_control.msg import TurtleVel
 from turtlesim.msg import Pose
 
-def move(vel_publish, direction):
-    cmd_vel = Twist()
+def poseCallback(position_message):
+    global x,y, yaw
+    x = position_message.x
+    y = position_message.y
+    yaw = position_message.theta
 
+def move(speed, distance, go_forward):
     global x,y
     x0 = x
     y0 = y
 
-    #init the cmd vel publisher using existing turtlesim topic cmd_vel and msg Twist
-    cmd_vel_pub = rospy.Publisher(cmd_vel, Twist, queue_size=10)
+    distance_moved = 0 #this will be updated in the while loop as the turtle moves
 
+
+    turtle_vel = "/turtle1/cmd_vel" #topic name for TurtleVel messages. turtle1 is just an instantiation of the turtlesim robot and cmd_vel is a preexisting msg in the turtlesim package
+    
+    #initialize the subscriber and publisher nodes on the same topic "turtle_vel" with same message type "TurtleVel"
+    loop_rate = rospy.Rate(10) #publish 10 message per second
+    turtle_vel_sub = rospy.Subscriber(turtle_vel, TurtleVel, poseCallback) #poseCallback will publish the change in position
+
+    #publisher node
+    turtle_vel_pub = rospy.Publisher(turtle_vel, TurtleVel, queue_size=10) #Twist is an existing msg file in turtlesim
+
+    #set the speed and direction of velocity command "TurtleVel"
+    if (go_forward):
+        TurtleVel.linear.x = abs(speed)
+    else:
+        TurtleVel.linear.x = -abs(speed)
+
+        
     while True:
-        rospy.loginfo("starting motion")
+        rospy.loginfo("moving forward")
+        
+        #prepare to move
+        #publish the speed and direction of turtle's motion using the "speed" and "direction" parameters passed into the move() method. Use them in the TurtleVel message
+        turtle_vel_pub.publish(TurtleVel)
 
-        cmd_vel_pub.publish()
+        loop_rate.sleep()
 
+        #use the distance equation to update the turtle's distance moved
+        distance_moved = abs(math.sqrt(((x-x0)**2) + ((y-y0)**2)))
+
+        rospy.loginfo("Turtle moved: ")
+        print(distance_moved)
+
+        #check for move stop condition
+        if (distance_moved>distance):
+            rospy.loginfo("destination reached")
+            break
+              
 
 
 if __name__ == '__main__':
@@ -26,13 +63,14 @@ if __name__ == '__main__':
 
         rospy.init_node('turtlesim_motion_pose', anonymous=True)
 
+        turtle_vel = "/turtle1/cmd_vel" #topic name for TurtleVel messages. turtle1 is just an instantiation of the turtlesim robot and cmd_vel is a preexisting msg in the turtlesim package
+        
         #subscriber node
-        turtle_vel = "/turtle1/pose" #topic name for subscriber message. turtle1 is just an instantiation of the turtlesim robot and pose is a preexisting msg in the turtlesim package
-        position_sub = rospy.Subscriber(turtle_vel, Pose, poseCallback) #Pose is an existing msg file in turtlesim
+        turtle_vel_sub = rospy.Subscriber(turtle_vel, TurtleVel, poseCallback) #Pose is an existing msg file in turtlesim
 
         #publisher node
-        turtle_interpret = "/turtle1/cmd_vel" #topic name for publisher message. turtle1 is just an instantiation of the turtlesim robot and cmd_vel is a preexisting msg in the turtlesim package
-        cmd_vel_pub = rospy.Publisher(turtle_interpret, Twist, queue_size=10) #Twist is an existing msg file in turtlesim
-    
+        turtle_vel_pub = rospy.Publisher(turtle_vel, TurtleVel, queue_size=10) #Twist is an existing msg file in turtlesim
+        time.sleep(2)
+
     except: rospy.ROSInterruptException:
         rospy.loginfo("node terminated")
