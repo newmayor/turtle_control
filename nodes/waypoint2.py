ac965022446e97162fdd0d43d643d156183beff5
Numair Ahmed <numairahmed1@gmail.com>
2021-01-24 00:06:14 -0800

    complete barebones structure of main function that will first define a publisher and subscriber and run a mover function to command the turtle.

--- nodes/waypoint2.py
+++ nodes/waypoint2.py
@@ -7,4 +7,17 @@ from geometry_msgs.msg import Twist
 
 if __name__ == '__main__':
     rospy.init_node('waypoint2')
-    
+
+    #define first topic. This will be main topic for cmd_vel commands
+    topic_cmd_vel = '/turtle1/cmd_vel'
+    #once turtlesim node is loaded, need to first publish command to move turtle
+    velocity_publisher = rospy.Publisher(topic_cmd_vel, Twist, queue_size=10)
+
+    #once turtle has moved, we need the position of the turtle. make a subscriber
+    topic_turtle_pose = '/turtle1/pose' #define the topic
+    subscriber_pose = rospy.Subscriber(topic_turtle_pose, Pose, poseCallback)
+
+    #define some kind of 'mover' function that will actually send desired positions to command the turtle to. 
+    mover()
+
+    print('waypoint moving of turtle1 is complete.')
